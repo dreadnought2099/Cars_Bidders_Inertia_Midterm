3b631228347a7d7edabf938d9d4a88a49732c1c1
Raymart Magallanes <magallanesraymart@gmail.com>
2025-02-13 23:32:47 +0800

    productcard changes

--- resources/js/Component/ProductCard.vue
+++ resources/js/Component/ProductCard.vue
@@ -10,29 +10,40 @@ defineProps({
 </script>
 
 <template>
-    <h1 class="text-center text-2xl font-bold text-dark mb-16">Famous Paintings
-        <div class="flex flex-wrap gap-6 justify-center">
-            <div
-                v-for="product in products"
-                :key="product.id"
-                class="bg-white p-5 rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 w-full max-w-xs flex flex-col cursor-pointer"
-            >
-                <h5 class="text-xl font-semibold text-dark-600 mb-2">
-                    {{ product.name }}
-                </h5>
-                <img :src="product.image" :alt="product.name" class="w-full h-screen object-cover">
-                <p class="text-base text-gray-700 mb-4 text-justify flex-grow">
-                    {{ product.description }}
-                </p>
-                <div class="flex justify-between text-sm mt-auto">
-                    <p class="text-red-500 font-semibold">
-                        Wholesale: ₱{{ product.whole_sale_price.toFixed(2) }}
+    <div class="py-12 px-6">
+        <h1 class="text-center text-4xl sm:text-5xl lg:text-6xl font-bold text-dark mb-16">
+            Famous Paintings
+        </h1>
+            <div class="flex flex-wrap gap-6 justify-center">
+                <div
+                    v-for="product in products"
+                    :key="product.id"
+                    class="bg-white p-5 rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 w-full sm:max-w-sm md:max-w-md lg:max-w-xs xl:max-w-sm transform hover:scale-105"
+                >
+                    <h5 class="text-xl font-semibold text-dark-600 mb-3">
+                        {{ product.name }}
+                    </h5>
+                    <img
+                        :src="product.image"
+                        :alt="product.name"
+                        class="w-full h-64 md:h-72 rounded-md mb-4 object-cover"
+                    />
+                    <p
+                        class="text-base text-gray-700 mb-4 text-justify flex-grow"
+                    >
+                        {{ product.description }}
                     </p>
-                    <p class="text-red-500 font-semibold">
-                        Retail: ₱{{ product.retail_price.toFixed(2) }}
-                    </p>
+                    <div class="flex justify-between text-sm mt-auto">
+                        <p class="text-dark font-semibold">
+                            Wholesale: ₱{{
+                                product.whole_sale_price.toFixed(2)
+                            }}
+                        </p>
+                        <p class="text-dark font-semibold">
+                            Retail: ₱{{ product.retail_price.toFixed(2) }}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </h1>
+    </div>
 </template>
